5e453d8db2338ccbd5e12314df48b2c5a7715c36
Ariko <ariko@vivaldi.net>
2021-02-19 12:45:41 +0100

    Add support for Pantalaimon for e2e encryption

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,11 @@ import * as config from "config";
 
 interface IConfig {
     homeserverUrl: string;
+    pantalaimon: {
+        use: boolean;
+        username: string;
+        password: string;
+    }
     accessToken: string;
     autoJoin: boolean;
     dataPath: string;
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {
     LogLevel,
     LogService,
     MatrixClient,
+    PantalaimonClient,
     RichConsoleLogger,
     SimpleFsStorageProvider
 } from "matrix-bot-sdk";
@@ -19,22 +20,31 @@ LogService.setLevel(LogLevel.DEBUG);
 // Print something so we know the bot is working
 LogService.info("index", "Bot starting...");
 
-// Prepare the storage system for the bot
-const storage = new SimpleFsStorageProvider(path.join(config.dataPath, "bot.json"));
-
-// Create the client
-const client = new MatrixClient(config.homeserverUrl, config.accessToken, storage);
-
-// Setup the autojoin mixin (if enabled)
-if (config.autoJoin) {
-    AutojoinRoomsMixin.setupOnClient(client);
-}
-
-// Prepare the command handler
-const commands = new CommandHandler(client);
 
 // This is the startup closure where we give ourselves an async context
 (async function () {
+    
+    // Prepare the storage system for the bot
+    const storage = new SimpleFsStorageProvider(path.join(config.dataPath, "bot.json"));
+    
+    // Create the client
+    let client: MatrixClient;
+    if (config.pantalaimon.use) { // create a client with Pantalaimon if pantalaimon.use set to true.
+        const pantalaimon = new PantalaimonClient(config.homeserverUrl, storage);
+        client = await pantalaimon.createClientWithCredentials(config.pantalaimon.username, config.pantalaimon.password);
+    } else { // else use Matrix client.
+        client = new MatrixClient(config.homeserverUrl, config.accessToken, storage);
+    }
+    
+    // Setup the autojoin mixin (if enabled)
+    if (config.autoJoin) {
+        AutojoinRoomsMixin.setupOnClient(client);
+    }
+    
+    // Prepare the command handler
+    const commands = new CommandHandler(client);
+    
+    
     await commands.start();
     LogService.info("index", "Starting sync...");
     await client.start(); // This blocks until the bot is killed
